efb79143315ae40e6b9b151051fad49ed27b1e90
Ashwin Sharma <ashwin19sharma@gmail.com>
2023-08-06 00:04:09 +0530

    added remove button in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import MenuListItems from "./MenuListItems";
 import { store } from "@reduxjs/toolkit";
 import { clearCart } from "../utils/cartSlice";
-
+import { CDN_IMG } from "../utils/constants";
+import { removeItems } from "../utils/cartSlice";
 const Cart = () => {
 
     const cartItems = useSelector((store) => store.cart.items)
@@ -13,6 +14,10 @@ const Cart = () => {
         dispatch(clearCart());
     }
 
+    const handleRemoveItem = () => {
+        dispatch(removeItems());
+    }
+
     return (
         <div className="text-center p-4 w-6/12  m-auto">
             <div className="flex items-center justify-around my-10">
@@ -21,7 +26,25 @@ const Cart = () => {
             </div>
             {cartItems.length === 0 && <h1 className="text-2xl font-semibold mt-20">Looks like your cart is empty. Please add items to proceed!!!</h1>}
             <div className="">
-                <MenuListItems itemsList={cartItems}/>
+                {cartItems.map((items) => (
+                    <div key={items.card.info.id} className="flex justify-between border-b-2 px-2 py-4">
+                        <div className="text-left w-9/12">
+                            <h2 className="text-lg">{items.card.info.name}</h2>
+                            <p className="text-md">₹ {items.card.info.price / 100 || items.card.info.defaultPrice / 100}</p>
+                            <p className="text-sm text-gray-400 py-4">{items.card.info.description}</p>
+                        </div>
+                        <div className="w-3/12 p-4">
+                            {items.card.info.imageId ? 
+                            <>
+                                <div className="absolute">
+                                    <button className="px-6 py-1 mx-4 my-20 font-semibold text-red-700 border border-red-700 rounded-md bg-white shadow-lg" onClick={handleRemoveItem}>REMOVE</button>
+                                </div>
+                                <img className="w-full h-24 rounded-lg" src={CDN_IMG+items.card.info.imageId}/>
+                            </> : 
+                                <button className="px-6 py-1 font-semibold text-red-700 border border-red-700 rounded-md bg-white shadow-lg" onClick={handleRemoveItem}>REMOVE</button>}
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
